8d9cf9d1b937c064840e0eee2c6641f945f8d6a5
fumi <fumi01200924@gmail.com>
2020-02-07 23:28:17 +0900

    OK

--- AkagiCrawler.py
+++ AkagiCrawler.py
@@ -2,45 +2,7 @@ import serial
 import time
 
 ser = serial.Serial("/dev/cu.usbserial-30", 115200,
-                    timeout=30, parity=serial.PARITY_NONE)
-flg = 1
-while flg:
-    moji = ser.readline()
-    if moji == b"Grbl 0.9i ['$' for help]\r\n":
-        print(moji.strip().decode('utf-8'))
-        flg = 0
-
-moji1 = 'M05 S0\r\n'
-moji2 = 'M03 S1000\r\n'
-
-time.sleep(1)
-
-print("Start")
-ser.write(b"M03 S0\r\n")
-print(moji1)
-flg = 1
-while flg:
-    tmp = ser.readline()
-    if tmp == b"ok\r\n":
-        print(tmp.strip().decode('utf-8'))
-        tmp2 = ser.readline()
-        if tmp2 == b"ok\r\n":
-            print(tmp2.strip().decode('utf-8'))
-            flg = 0
-
-ser.write(b"M03 S1000\r\n")
-print(moji2)
-flg = 1
-while flg:
-    tmp = ser.readline()
-    if tmp == b"ok\r\n":
-        print(tmp.strip().decode('utf-8'))
-        tmp2 = ser.readline()
-        if tmp2 == b"ok\r\n":
-            print(tmp2.strip().decode('utf-8'))
-            flg = 0
-
-ser.close()
+                   timeout = 30, parity=serial.PARITY_NONE)
 
 
 def checkOK():
@@ -51,17 +13,38 @@ def checkOK():
             print(tmp.strip().decode('utf-8'))
             tmp2 = ser.readline()
             if tmp2 == b"ok\r\n":
-                print(tmp2.strip().decode('utf-8'))
                 flg = 0
 
 
 def servoUp():
     data = b"M03 S0\r\n"
+    print("data")
     ser.write(data)
     checkOK()
 
 
 def servoDown():
     data = b"M03 S1000\r\n"
+    print("data")
     ser.write(data)
     checkOK()
+
+
+def main():
+    flg = 1
+    while flg:
+        moji = ser.readline()
+        if moji == b"Grbl 0.9i ['$' for help]\r\n":
+            print(moji.strip().decode('utf-8'))
+            flg = 0
+    time.sleep(1)
+    print("Start")
+    servoUp()
+    time.sleep(1)
+    servoDown()
+    time.sleep(1)
+    ser.close()
+
+
+if __name__ == '__main__':
+    main()
