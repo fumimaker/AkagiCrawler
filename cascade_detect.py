96e4df004067daf33d59973bb3d28c279d29ce8b
fumi <fumi01200924@gmail.com>
2020-02-20 11:59:06 +0900

    カスケード無理そう

--- cascade_detect.py
+++ cascade_detect.py
@@ -1,18 +1,28 @@
 import cv2
-import numpy as np
 
-# 学習器(cascade.xml)の指定
-Cascade = cv2.CascadeClassifier('./cv/cascade/data/stage10.xml')
-# 予測対象の画像の指定
-img = cv2.imread('test.jpg', cv2.IMREAD_COLOR)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-point = Cascade.detectMultiScale(gray, 1.1, 3)
+image = "./test2.PNG"
+# カスケードファイルを読み込む
+face_cascade = cv2.CascadeClassifier(
+    "./stage4.xml")
+# 画像を読み込む
+loaded_img = cv2.imread(f"{image}")
+# （高さ・幅・色数)
+# 読み込んだ画像を、グレースケール画像として読み込む
+gray = cv2.cvtColor(loaded_img, cv2.COLOR_BGR2GRAY)
 
-if len(point) > 0:
-    for rect in point:
-        cv2.rectangle(img, tuple(rect[0:2]), tuple(
-            rect[0:2]+rect[2:4]), (0, 0, 255), thickness=2)
+faces = face_cascade.detectMultiScale(gray)
+if len(faces) == 0:
+    print('画像が認識されません')
 else:
-    print("not detected.")
-
-cv2.imwrite('detected.jpg', img)
+    for (x, y, w, h) in faces:
+            # この中で、顔の部分に色をつけている処理をしている
+        penned_img = cv2.rectangle(
+            loaded_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = penned_img[y:y+h, x:x+w]
+    # 画像を表示させる
+    cv2.imshow('img', penned_img)
+    # キーボードの入力を待つ
+    cv2.waitKey(0)
+    # 画像windowを閉じる
+    cv2.destroyAllWindows()
